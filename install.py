09618021f156a134953571d92dbc10bdad94f6e0
Mike Atkins <matkins@ngpvan.com>
2013-07-29 14:33:32 -0400

    Fixed path issue

--- install.py
+++ install.py
@@ -6,7 +6,8 @@ whitelist = ["Xdefaults", "bash_profile", "bashrc", "config", "gvimrc",
     "inputrc", "jshintrc", "tmux.conf", "vim", "vimrc", "xmonad"]
 
 homeDir = os.path.expanduser("~")
+currentFileDirectory = os.path.dirname(os.path.realpath(__file__))
 
 for file in whitelist:
     dest = homeDir + "/." + file
-    os.symlink(file, dest)
+    os.symlink(currentFileDirectory + "/" + file, dest)
