6f4c5ae281a9dbdaea049cbcd8571b4f61b03e72
Mike Zorn <mike@launchdarkly.com>
2019-09-16 15:30:20 -0700

    update misc fish plugins

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,10 @@
 eval (python -m virtualfish compat_aliases)
+gvm 1.12.9 | source
 
 alias vi nvim
 alias vim nvim
 
 ulimit -n 5000
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/mikezorn/google-cloud-sdk/path.fish.inc' ]; . '/Users/mikezorn/google-cloud-sdk/path.fish.inc'; end
