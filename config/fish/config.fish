915d7e1446229f50f6b3ce5c12f658b38dd4fea7
Mike Zorn <mike@launchdarkly.com>
2020-08-16 17:30:22 -0700

    Save aliases correctly

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,7 @@
 eval (python -m virtualfish compat_aliases)
-gvm 1.13.1 | source
-
-alias vi nvim
-alias vim nvim
+gvm 1.14.3 | source
 
 ulimit -n 5000
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/mikezorn/google-cloud-sdk/path.fish.inc' ]; . '/Users/mikezorn/google-cloud-sdk/path.fish.inc'; end
-alias cat bat
-alias vsp "nvr -cc ':vsp'"
---
+++ config/fish/functions/stopld.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function stopld --description 'alias stopld kill'
+	kill  $argv;
+end
